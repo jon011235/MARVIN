a0fa72efca127d3b3a94a1b5c88334693439a9e8
Leonard Pfeiffer <leonard.s.pfeiffer@gmx.net>
2023-10-28 03:23:13 +0200

    Create quartus project
    - Add toplevel entity
    - Add toplevel package
    - Add quartus project
    - Add pin assignments
    - Remove quartus dbs and outputs from gitignore

---
+++ src/pkg.sv
@@ -0,0 +1,12 @@
+/*
+    Project:    MARVIN
+    Sector:     TOP
+    Entity:     pkg
+    Summary:    Toplevel package file
+    Device:     terasIC® DE10-Lite Development Board with Altera® MAX 10 10M50DAF484C7G FPGA
+    Authors:    Leonard Pfeiffer
+*/
+
+package pkg;
+    typedef struct { logic [3:0] red, green, blue; } color;
+endpackage
