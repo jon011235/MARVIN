73ecc1cb35b85d1f05a16c82bcefbcee48e6c8d7
Leonard Pfeiffer <leonard.s.pfeiffer@gmx.net>
2023-10-29 10:56:44 +0100

    Add counter logic

--- src/utils/counter.sv
+++ src/utils/counter.sv
@@ -9,7 +9,18 @@ module counter #(
     parameter T
 )(
     input clk, rst,
-    output [$clog2(T)-1:0] count
+    output [$clog2(T) - 1 : 0] cnt
 );
+    reg cntr;
 
+    always @(posedge clk, posedge rst)
+        if (rst)
+            cntr = '0;
+        else
+            if (cntr < T - 1)
+                cntr++;
+            else
+                cntr = '0;
+    
+    assign cnt = cntr;
 endmodule : counter
