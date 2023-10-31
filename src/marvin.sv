f8159dc6056e7cdfc2f440663a1daaf09df1a408
Leonard Pfeiffer <leonard.s.pfeiffer@gmx.net>
2023-10-31 22:12:41 +0100

    Improve toplevel structure

--- src/marvin.sv
+++ src/marvin.sv
@@ -1,9 +1,8 @@
 /*
     Project:    MARVIN
     Sector:     TOP
-    Summary:    Toplevel design file
+    Summary:    Toplevel design file, dedicated to pll instantiations and io-formatting
     Device:     terasIC® DE10-Lite Development Board with Altera® MAX 10 10M50DAF484C7G FPGA
-    Authors:    Leonard Pfeiffer
 */
 
 import pkg::*;
@@ -17,36 +16,36 @@ module marvin(
     input clk2_50,                  // 50MHz secondary clock    3.3V LVTTL
 
     // ===== BASIC IN ====================================================
-    input [1:0] key_,               // 2 push buttons           3.3V Schmitt
+    input [1:0] key_,               // Push buttons             3.3V Schmitt
 
-    input [9:0] sw,                 // 10 toggle switches       3.3V LVTTL
+    input [9:0] sw,                 // Toggle switches          3.3V LVTTL
 
     // ===== BASIC OUT ===================================================
-    output [9:0] ledr,              // 10 red leds              3.3V LVTTL
+    output [9:0] ledr,              // Red leds                 3.3V LVTTL
 
-    output pkg::seg7p_t [5:0] hex_, // 6x8-element hex display  3.3V LVTTL
+    output pkg::seg7p_t [5:0] hex_, // 8-element hex displays   3.3V LVTTL
 
     // ===== BASIC IO ====================================================
-    inout [35:0] gpio,              // 36 pin expansion header  3.3V LVTTL
+    inout [35:0] gpio,              // Expansion header         3.3V LVTTL
 
     // ===== ARDUINO =====================================================
-    inout [15:0] ardu_gpio,         // 16 pin arduino connector 3.3V LVTTL
+    inout [15:0] ardu_gpio,         // Arduino connector        3.3V LVTTL
     inout ardu_rst_,                // Arduino reset            3.3V Schmitt
 
     // ===== VGA =========================================================
-    output pkg::color_t vga_color,    // 3x4 VGA color output     3.3V LVTTL
+    output pkg::color_t vga_color,    // VGA color output       3.3V LVTTL
     output vga_hs,                  // VGA horizontal sync      3.3V LVTTL
     output vga_vs,                  // VGA vertical sync        3.3V LVTTL
 
     // ===== SDRAM =======================================================
-    output [12:0] dram_addr,        // 13 bit SDRAM address     3.3V LVTTL
-    inout [15:0] dram_dq,           // 16 bit SDRAM data bus    3.3V LVTTL
-    output [1:0] dram_bank,         // 2 bit SDRAM bank address 3.3V LVTTL
-    output [1:0] dram_qdm,          // 2 bit SDRAM bit mask     3.3V LVTTL
+    output [12:0] dram_addr,        // SDRAM address            3.3V LVTTL
+    inout [15:0] dram_dq,           // SDRAM data bus           3.3V LVTTL
+    output [1:0] dram_bank,         // SDRAM bank address       3.3V LVTTL
+    output [1:0] dram_qdm,          // SDRAM bit mask           3.3V LVTTL
     output dram_ras_,               // SDRAM row address strobe 3.3V LVTTL
     output dram_cas_,               // SDRAM col address strobe 3.3V LVTTL
     output dram_cke,                // SDRAM clock enable       3.3V LVTTL
-    output dram_clk,                // 200MHz SDRAM clock       3.3V LVTTL
+    output dram_clk,                // SDRAM clock              3.3V LVTTL
     output dram_re,                 // SDRAM read enable        3.3V LVTTL
     output dram_cs_,                // SDRAM chip select        3.3V LVTTL
 
@@ -57,31 +56,39 @@ module marvin(
     output gsensor_sclk,            // I2C / SPI serial clock   3.3V LVTTL
     input [2:1] gsensor_int         // GSensor interrupt pins   3.3V LVTTL
 );
-    // ===== BASIC ============
-    assign ledr = '0;
-    assign hex_ = '1;
-    assign gpio = '0;
-    assign ardu_gpio = '0;
-    assign ardu_rst_ = 0;
-    assign vga_color = '0;
-    assign vga_hs = 0;
-    assign vga_vs = 0;
+    sysbus bus (
+        .clk_10(clk_10),
+        .clk1_50(clk1_50),
+        .clk2_50(clk2_50),
+        .rst(~key_[0] || ~ardu_rst_),
+        .btn(~key_[1]),
+        .sw(sw),
+        .led(ledr),
+        .hex_(hex_),
+        .gpio(gpio),
+        .ardu_gpio(ardu_gpio),
 
-    // ===== SDRAM ============
-    assign dram_addr = '0;
-    assign dram_dq = '0;
-    assign dram_bank = '0;
-    assign dram_qdm = '0;
-    assign dram_cas_ = 0;
-    assign dram_ras_ = 0;
-    assign dram_cke = 0;
-    assign dram_clk = 0;
-    assign dram_re = 0;
-    assign dram_cs_ = 1;
+        .vga_color(vga_color),
+        .vga_hs(vga_hs),
+        .vga_vs(vga_vs),
 
-    // ===== GSENSOR ==========
-    assign gsensor_sdi = 0;
-    assign gsensor_sdo = 0;
-    assign gsensor_cs_ = 1;
-    assign gsensor_sclk = 0;
+        .dram_addr(dram_addr),
+        .dram_dq(dram_dq),
+        .dram_bank(dram_bank),
+        .dram_qdm(dram_qdm),
+        .dram_ras_(dram_ras_),
+        .dram_cas_(dram_cas_),
+        .dram_cke(dram_cke),
+        .dram_clk(dram_clk),
+        .dram_re(dram_re),
+        .dram_cs_(dram_cs_),
+
+        .gsensor_sdi(gsensor_sdi),
+        .gsensor_sdo(gsensor_sdo),
+        .gsensor_cs_(gsensor_cs_),
+        .gsensor_sclk(gsensor_sclk),
+        .gsensor_int(gsensor_int)
+    );
+
+    assign ardu_rst_ = 'z;
 endmodule
---
+++ src/sysbus.sv
@@ -0,0 +1,76 @@
+/*
+    Project:    MARVIN
+    Sector:     TOP
+    Summary:    System-bus file, dedicated to intermodular communication
+*/
+
+import pkg::*;
+
+module sysbus(
+    input clk_10, clk1_50, clk2_50, // Clocks
+
+    input rst,                      // Reset
+
+    input btn,                      // Push-button
+
+    input [9:0] sw,                 // Toggle switches
+
+    output [9:0] led,               // Leds
+
+    output pkg::seg7p_t [5:0] hex_, // 8-element hex displays
+
+    inout [35:0] gpio,              // GPIO pins
+
+    inout [15:0] ardu_gpio,         // Arduino connector
+
+    // ===== VGA =========================================================
+    output pkg::color_t vga_color,  // VGA color output
+    output vga_hs,                  // VGA horizontal sync
+    output vga_vs,                  // VGA vertical sync
+
+    // ===== SDRAM =======================================================
+    output [12:0] dram_addr,        // SDRAM address
+    inout [15:0] dram_dq,           // SDRAM data bus
+    output [1:0] dram_bank,         // SDRAM bank address
+    output [1:0] dram_qdm,          // SDRAM bit mask
+    output dram_ras_,               // SDRAM row address strobe
+    output dram_cas_,               // SDRAM col address strobe
+    output dram_cke,                // SDRAM clock enable
+    output dram_clk,                // SDRAM clock
+    output dram_re,                 // SDRAM read enable
+    output dram_cs_,                // SDRAM chip select
+
+    // ===== GSENSOR =====================================================
+    inout gsensor_sdi,              // I2C D or SPI I 4 / IO 3
+    inout gsensor_sdo,              // SPI O 4 / Alt I2C Addr
+    output gsensor_cs_,             // I2C / SPI Mode
+    output gsensor_sclk,            // I2C / SPI serial clock
+    input [2:1] gsensor_int         // GSensor interrupt pins
+);
+    // ===== BASIC ============
+    assign led = '0;
+    assign hex_ = '0;
+    assign gpio = 'z;
+    assign ardu_gpio = 'z;
+    assign vga_color = '0;
+    assign vga_hs = 0;
+    assign vga_vs = 0;
+
+    // ===== SDRAM ============
+    assign dram_addr = '0;
+    assign dram_dq = 'z;
+    assign dram_bank = '0;
+    assign dram_qdm = '0;
+    assign dram_cas_ = 0;
+    assign dram_ras_ = 0;
+    assign dram_cke = 0;
+    assign dram_clk = 0;
+    assign dram_re = 0;
+    assign dram_cs_ = 0;
+
+    // ===== GSENSOR ==========
+    assign gsensor_sdi = 'z;
+    assign gsensor_sdo = 'z;
+    assign gsensor_cs_ = 0;
+    assign gsensor_sclk = 0;
+endmodule
