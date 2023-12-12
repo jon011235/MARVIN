cfd1838e14a62bb8351020b7e7e4323258dee7af
Leonard Pfeiffer <leonard.s.pfeiffer@gmx.net>
2023-12-12 11:59:04 +0100

    Add uart ports

--- src/com/uart.sv
+++ src/com/uart.sv
@@ -6,9 +6,17 @@
 */
 
 module uart #(
-
+    parameter CLK_ natural = 50000000,
+    parameter BUFF_ natural = 64,
+    parameter BAUD_ natural = 115200,
+    parameter DATA_ natural = 8,
+    parameter STOP_ natural = 1,
+    parameter PARITY string  = "none"   // even, odd, mark, space
 )(
-
+    input clk, rst_, we, re, rx
+    input [DATA_ - 1 : 0],
+    output [DATA_ - 1 : 0],
+    output tx
 );
 
 endmodule
--- src/utils/ram/shiftbram.sv
+++ src/utils/ram/shiftbram.sv
@@ -9,7 +9,7 @@ module shiftbram #(
     parameter int unsigned DATA_ = 8,
     parameter int unsigned ADDR_ = 8
 )(
-    input clk, cke, aclr, 
+    input clk, ena, aclr, 
     input [DATA_ - 1 : 0] din,
     output [DATA_ - 1 : 0] dout,
     output [2**ADDR_ - 1 : 0] [DATA_ - 1 : 0] taps
@@ -23,7 +23,7 @@ module shiftbram #(
         .width(DATA_)
     ) shift (
         .clock(clk),
-        .clken(cke),
+        .clken(ena),
         .aclr(aclr),
         .shiftin(din),
         .shiftout(dout),
